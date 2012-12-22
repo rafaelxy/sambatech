ba179ced4b68e24e2e24ac45385e8ddec23836d3
rafaelxy <rafaelxy@gmail.com>
2012-12-22 20:09:50 -0200

    DONE: IO webcam -> disco

--- ap2_webcam/src/main.py
+++ ap2_webcam/src/main.py
@@ -4,5 +4,16 @@ Created on 22 Dec 2012
 @author: rcampos
 '''
 
-if __name__ == '__main__':
-    print "hello world"
+import cv2.cv as cv
+
+capture=cv.CaptureFromCAM(0)
+image=cv.QueryFrame(capture)
+writer=cv.CreateVideoWriter("output-webcam.avi", 0, 8.0, cv.GetSize(image), 1)
+
+count=0
+while count<100:
+    image=cv.QueryFrame(capture)
+    cv.WriteFrame(writer, image)
+    cv.ShowImage('Image_Window',image)
+    cv.WaitKey(1)
+    count+=1
